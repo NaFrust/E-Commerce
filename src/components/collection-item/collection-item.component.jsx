03c700c1f651e681c5991518d0a6073438d7c6bb
Nahuel Frustaglia <nahuel.frustaglia@gmail.com>
2021-08-16 00:52:21 -0300

    se modifico shop data, se actualizo item colection para poder funcionar

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import './collection-item.style.scss';
+import { connect } from 'react-redux';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className='collection-item'>
+import './collection-item.style.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+
+const CollectionItem = ({item, addItem}) => {
+   const {name, price, imageUrl} = item;
+   return(
+   <div className='collection-item'>
         <div className='image'
             style={{
                 backgroundImage:`url(${imageUrl})`
@@ -11,7 +17,13 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton onClick={()=> addItem(item)}  inverted>Agregar al Carro</CustomButton>
     </div>
-)
+    )
+}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch =>({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null,mapDispatchToProps )(CollectionItem);
